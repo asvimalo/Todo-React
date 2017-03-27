4167cb828c68efae5cfb7bf459098fed83943d78
circoloco <asvimalo@gmail.com>
2017-03-27 11:26:54 +0200

    List first todos

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route,Router, IndexRoute, hashHistory} = require('react-router');
 
-var Main = require('./components/Main');
+var TodoApp = require('TodoApp');
 
 
 // Load foundation
@@ -14,7 +14,7 @@ $(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Main}>
+    <Route path="/" component={TodoApp}>
 
     </Route>
   </Router>,
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,38 @@
+const React = require('react');
+const TodoList = require('TodoList');
+
+var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+      {
+        id: 1,
+        text: 'Walk the dog'
+      },
+      {
+        id: 2,
+        text: 'Clean the Yard'
+      },
+      {
+        id: 3,
+        text: 'Leave mail on porch'
+      },
+      {
+        id: 4,
+        text: 'Play video games'
+      }
+      ]
+    }
+  },
+  render: function () {
+    var {todos} = this.state;
+
+    return (
+      <div>
+        <TodoList todos={todos} />
+      </div>
+    )
+  }
+})
+
+module.exports = TodoApp;
